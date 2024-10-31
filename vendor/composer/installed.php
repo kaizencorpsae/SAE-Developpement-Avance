806141c057facddef9fee0dfed1167a805470089
Nathan Boulangeot <boulangeot.nathan@gmail.com>
2024-10-31 11:55:46 +0100

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'boula/sae',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9a81f7aef3c6e53d509edd7a4f0f1d867f485f25',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'boula/sae' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9a81f7aef3c6e53d509edd7a4f0f1d867f485f25',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
