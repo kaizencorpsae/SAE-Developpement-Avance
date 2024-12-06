94e1f95b2c005f611bf6661e773f8b23680b5c18
Nathan Boulangeot <boulangeot.nathan@gmail.com>
2024-12-06 15:00:28 +0100

    Update pages, vues et images

--- resources/views/begin.blade.php
+++ resources/views/begin.blade.php
@@ -3,6 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Résultats de recherche</title>
+    <title>Let Me Cook</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
+
+<header class="bg-gray-900 py-6 shadow-lg">
+    <div class="max-w-7xl mx-auto px-4 flex items-center justify-between">
+        <a href="/" class="flex items-center space-x-4">
+            <img src="/images/letmecooklogocomplete.png" alt="Logo" class="w-12 h-12 rounded-full">
+            <h1 class="text-3xl font-semibold">Liste des Plats</h1>
+        </a>
+        <nav>
+            <a href="https://github.com/kaizencorpsae/SAE-Developpement-Avance" class="text-blue-400 hover:text-blue-500">Voir le GitHub</a>
+        </nav>
+    </div>
+</header>
--- resources/views/plats.blade.php
+++ resources/views/plats.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Liste des Plats</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.0/dist/tailwind.min.css" rel="stylesheet">
-</head>
+@include('begin')
 <body class="bg-gray-800 text-white">
 
 <header class="text-center py-6">
