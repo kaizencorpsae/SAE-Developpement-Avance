79dad8ece088738355ce2b7203339c2b29b03697
Nathan Boulangeot <boulangeot.nathan@gmail.com>
2024-12-13 16:00:59 +0100

    Update css & autres

--- resources/views/begin.blade.php
+++ resources/views/begin.blade.php
@@ -8,17 +8,31 @@
     <link href="/resources/" rel="stylesheet">
     <link href="/css/main.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-duotone-solid.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-thin.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-solid.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-regular.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-light.css">
 </head>
+
+<body class="bg-main-800 text-white">
 
 <header class="bg-main-600 header py-2 shadow-lg">
     <div class="max-w-7xl mx-auto px-4 flex items-center justify-between">
-        <a href="/" class="flex items-center space-x-4">
+        <a href="/" class="link flex items-center space-x-4">
             <img src="/images/letmecooklogocomplete.png" alt="Logo" class="w-16 h-16">
-            <h1 class="text-3xl font-semibold">Liste des Plats</h1>
+            <h1 class="text-3xl font-semibold">LetMeCook</h1>
         </a>
-        <nav>
-            <a href="https://github.com/kaizencorpsae/SAE-Developpement-Avance" class="text-blue-400 hover:text-blue-500">Voir le GitHub</a>
+
+        <nav class="flex items-center justify-between space-x-8 py-4">
+            <a href="/plats" class="link flex items-center">
+                <h1 class="text-2xl font-semibold text-white">Plats</h1>
+            </a>
+
+            <a href="https://github.com/kaizencorpsae/SAE-Developpement-Avance"
+               class="mainbtn text-white py-1 px-1 rounded-lg flex items-center space-x-2 lg-rounded fa-brands fa-github text-3xl">
+            </a>
         </nav>
     </div>
 </header>
-<body class="bg-main-800 text-white">
