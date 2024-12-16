b497523e1a24914b1f1e51705f0debf2bcb16471
Alexandre-Meunier <alexaudmeunier@outlook.fr>
2024-12-16 14:27:01 +0100

    Update footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="text-center py-6 bg-main-650">
-    <p class="text-main-600">&copy; 2024 SAE - Tous droits réservés.</p>
+    <p class="text-main-600">&copy; 2024 KaizenCorp - Tous droits réservés.</p>
 </footer>
 
 </body>
