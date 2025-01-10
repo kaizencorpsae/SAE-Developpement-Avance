e5dab5fe6fc8b872f59a7303087717c4b999608b
alexandre-meunier <alexaudmeunier@outlook.fr>
2025-01-10 16:40:47 +0100

    [crud] Add route create

--- resources/views/plat/create.blade.php
+++ resources/views/plat/create.blade.php
@@ -1,8 +1,23 @@
-<form action="{{route('plats.store')}}" method="post">
-     @csrf
-    <input type="text" name="nom"> Titre
-    <br>
-    <input type="text" name="description" id=""> Description
-    <br>
-    <input type="submit" value="">Envoyer
+@include('begin')
+
+<header class="bg-main-750 text-center py-6 pt-28">
+    <h1 class="text-3xl font-semibold">Créer un plat</h1>
+</header>
+
+<div>
+    <form action="{{route('plats.store')}}" method="post">
+         @csrf
+        <label for="nom">Nom du plat :</label>
+        <input type="text" id="nom" name="nom" placeholder="Ex : pizza saumon" required>
+        <br>
+        <label for="nom">Description du plat :</label>
+        <input type="text" id="description" name="description" placeholder="Ex : pizza avec du saumon" required>
+        <br>
+        <label for="preparation">Votre préparation:</label>
+        <textarea name="preparation" id="preparation" required></textarea>
+        <br>
+        <input type="submit" value="Envoyer">
 </form>
+</div>
+
+@include('footer')
